b304c4ed2bc95dfa19275ed925c09f22757f2315
Alan Lu <alanlu1023@gmail.com>
2019-03-01 17:56:22 -0600

    Optimize addWithOverflow

--- app/src/utils/solidity.js
+++ app/src/utils/solidity.js
@@ -1,6 +1,6 @@
 import web3 from "web3";
 
-const { isBN, toBN, BN, leftPad } = web3.utils;
+const { toBN, leftPad } = web3.utils;
 
 export const normHex = i => (`${i}`.startsWith("0x") ? i.slice(2) : i);
 
@@ -8,18 +8,4 @@ export const asAddress = adr => `0x${leftPad(normHex(adr.toString(16)), 40)}`;
 
 export const asBytes32 = num => `${leftPad(num.toString(16), 64)}`;
 
-export const addWithOverflow = (a, b) => {
-  const aBN = isBN(a) ? a : toBN(a);
-  const bBN = isBN(b) ? b : toBN(b);
-
-  const aBinary = aBN.toString(2).slice(-256);
-  const bBinary = bBN.toString(2).slice(-256);
-
-  const product = new BN(aBinary, 2).add(new BN(bBinary, 2));
-
-  const productTrunctated = new BN(product.toString(2), 2)
-    .toString(2)
-    .slice(-256);
-
-  return new BN(productTrunctated, 2);
-};
+export const addWithOverflow = (a, b) => toBN(a).add(toBN(b)).maskn(256)

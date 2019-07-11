5ff5e2f4e7418b553df462dbe50239f9e3286f38
Leopold Joy <leo@leopoldjoy.com>
2019-07-11 14:15:02 +0100

    setup mock data for FCLA markets

--- migrations/utils/deploy-config.js
+++ migrations/utils/deploy-config.js
@@ -1,7 +1,14 @@
 // default to 30 days later
 const defaultResolutionTime = Math.floor(Date.now() / 1000) + 30 * 24 * 60 * 60;
 
+// default dates for mock API values
+const beginning2020 =
+  Math.floor(Date.now() / 1000) + (new Date(2020, 1, 1) - Date.now()) / 1000;
+const beginning2021 =
+  Math.floor(Date.now() / 1000) + (new Date(2021, 1, 1) - Date.now()) / 1000;
+
 module.exports = {
+  // Dai Deployment Configs
   daiPriceResolutionTime:
     process.env.DAIPRICERESOLUTIONTIME || defaultResolutionTime,
   daiPriceTargetValue: process.env.DAIPRICETARGETVALUE || "1" + "0".repeat(18),
@@ -19,5 +26,18 @@ module.exports = {
     process.env.DAISTABILITYFEETARGETVALUE || "1000000005113779426955452540",
   daiStabilityFeeQuestionID: process.env.DAISTABILITYFEEQUESTIONID || "0x03",
 
-  ammFunding: process.env.AMMFUNDING || "1000" + "0".repeat(18)
+  ammFunding: process.env.AMMFUNDING || "1000" + "0".repeat(18),
+
+  // For mock API values
+  ecoTreeRegistrationTargetValue:
+    process.env.ECOTREEREGISTRATIONTARGETVALUE || 10000,
+  ecoTreeRegistrationTargetTime:
+    process.env.ECOTREEREGISTERATIONTARGETTIME || beginning2020,
+
+  iceAliveMeltRateTarget: process.env.ICEALIVEMELTRATETARGET || 1.2,
+  iceAliveMeltRateTargetTime:
+    process.env.ICEALIVEMELTRATETARGETTIME || beginning2021,
+
+  AXAParametricInsuranceTargetValue: 100000,
+  AXAParametricInsuranceTargetTime: beginning2020
 };
